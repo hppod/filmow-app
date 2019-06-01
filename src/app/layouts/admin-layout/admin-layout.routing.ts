e6cd81ee5fa382be46410fccb1e647b9578e900d
hppod <hppodgornik@gmail.com>
2019-06-01 19:12:00 -0300

    Redirect to home router

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -4,6 +4,7 @@ import { MoviesComponent } from './../../movies/movies.component'
 import { MovieComponent } from './../../movie/movie.component'
 
 export const AdminLayoutRoutes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'movies', component: MoviesComponent },
     { path: 'movie/:id', component: MovieComponent }
