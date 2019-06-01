9211f2d8c9c459a2798e1a0febeefd336aefe2e2
hppod <hppodgornik@gmail.com>
2019-06-01 18:14:14 -0300

    Creating movies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   AgmCoreModule
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   imports: [
@@ -27,6 +28,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    MoviesComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
