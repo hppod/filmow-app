db6dcbf0f03fe6476135913ab5b1540fe1782e8b
hppod <hppodgornik@gmail.com>
2019-06-02 18:30:16 -0300

    Binding data on card actor component

--- src/app/movie/actors/card-actor/card-actor.component.ts
+++ src/app/movie/actors/card-actor/card-actor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Actor } from "./../actor.model"
 
 @Component({
   selector: 'app-card-actor',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-actor.component.scss']
 })
 export class CardActorComponent implements OnInit {
+
+  @Input() actor: Actor
 
   constructor() { }
 
