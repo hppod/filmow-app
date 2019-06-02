ef24cf3616e63ff2170f39d21c22e3909cebdea1
hppod <hppodgornik@gmail.com>
2019-06-02 15:52:38 -0300

    Layout receive info movies

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movie } from './../movies/movie.model'
 import { MovieService } from './movie.service'
 import { ActivatedRoute } from "@angular/router"
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-movie',
@@ -11,13 +12,15 @@ import { ActivatedRoute } from "@angular/router"
 export class MovieComponent implements OnInit {
 
   movie: Movie
+  trailer: any
 
-  constructor(private ms: MovieService, private route: ActivatedRoute) { }
+  constructor(private ms: MovieService, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     const id: number = this.route.snapshot.params['id']
     this.ms.getMovie(id).subscribe((response) => {
       this.movie = response
+      this.trailer = this.sanitizer.bypassSecurityTrustResourceUrl(response.TRAILER_URL)
       console.log(response)
     })
   }
