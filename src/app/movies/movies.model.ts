f1cd6b868486e90f7a038a0e2027cd60f3e8cb56
hppod <hppodgornik@gmail.com>
2019-06-02 13:08:58 -0300

    Remove comma from interface

--- src/app/movies/movies.model.ts
+++ src/app/movies/movies.model.ts
@@ -1,11 +1,11 @@
 export interface Movies {
-    id: number,
-    title: string,
-    date_premiere: string,
-    age: number,
-    duration: string,
-    genre: string,
-    storyline: string,
-    poster_url: string,
+    id: number
+    title: string
+    date_premiere: string
+    age: number
+    duration: string
+    genre: string
+    storyline: string
+    poster_url: string
     trailer_url: string
 }
