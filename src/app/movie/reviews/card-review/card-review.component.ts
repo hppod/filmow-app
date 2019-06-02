340f862d2d5d6c8b313aaf8d9385ebe9496eb9f6
hppod <hppodgornik@gmail.com>
2019-06-02 20:39:34 -0300

    Implement card review

--- src/app/movie/reviews/card-review/card-review.component.ts
+++ src/app/movie/reviews/card-review/card-review.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Review } from "./../review.model"
 
 @Component({
   selector: 'app-card-review',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-review.component.scss']
 })
 export class CardReviewComponent implements OnInit {
+
+  @Input() review: Review
 
   constructor() { }
 
