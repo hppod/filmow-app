1dd5ba469d5248282566b300570a59e3bf0d6e02
hppod <hppodgornik@gmail.com>
2019-06-02 20:30:08 -0300

    Implement service get Reviews

---
+++ src/app/movie/reviews/reviews.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core"
+import { HttpClient } from "@angular/common/http"
+import { Observable } from "rxjs"
+import { Review } from "./review.model"
+import { Filmow_API } from "./../../app.api"
+
+@Injectable()
+export class ReviewsService {
+
+    constructor(private http: HttpClient) { }
+
+    getReviews(id: number): Observable<Review[]> {
+        return this.http.get<Review[]>(`${Filmow_API}/movie-reviews/${id}`)
+    }
+}
