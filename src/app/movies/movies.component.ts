cda018cd77f0f67afec69307e9909d442baab844
hppod <hppodgornik@gmail.com>
2019-06-16 14:32:25 -0300

    Implement filter and show

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from "./movies.service"
 import { Movie } from "./movie.model"
 import { PaginationService } from 'app/components/pagination/pagination.service';
+import { HttpParams } from "@angular/common/http"
 
 @Component({
   selector: 'app-movies',
@@ -15,13 +16,18 @@ export class MoviesComponent implements OnInit {
   numberOfPages: number[] = []
   column: string = 'DATE_PREMIERE'
   order: string = 'DESC'
-  genre: string
 
   options: any[] = [
     { OPTION: 'Nome: A - Z', COLUMN: 'TITLE', ORDER: 'ASC' },
     { OPTION: 'Nome: Z - A', COLUMN: 'TITLE', ORDER: 'DESC' },
     { OPTION: 'Lançamento: Menor - Maior', COLUMN: 'DATE_PREMIERE', ORDER: 'ASC' },
     { OPTION: 'Lançamento: Maior - Menor', COLUMN: 'DATE_PREMIERE', ORDER: 'DESC' },
+  ]
+
+  shows: any[] = [
+    { OPTION: 'Todos', SHOW: 'IS NOT NULL' },
+    { OPTION: 'Em cartaz', SHOW: '<now()' },
+    { OPTION: 'Em breve', SHOW: '>now()' }
   ]
 
   genres: any[] = []
@@ -33,14 +39,15 @@ export class MoviesComponent implements OnInit {
 
     this.ms.getGenres().subscribe((response) => {
       this.genres = response
+      this.genres.unshift({ GENRE: 'Todos' })
     })
 
     this.findMovies(this.ps.currentPage, this.column, this.order)
+
   }
 
   findMovies(currentPage: number, column: string, order: string) {
     this.ms.getInfoMovies(currentPage, column, order).subscribe((response) => {
-      console.log(response)
       this.movies = response['result']
       this.pages = response['pages']
       this.setNumberOfPages()
@@ -72,9 +79,22 @@ export class MoviesComponent implements OnInit {
   }
 
   setFilter(option) {
-    this.genre = option
+    let genre: string
+
     this.ps.currentPage = 1
 
+    if (option == 'Todos') {
+      genre = 'IS NOT NULL'
+    } else {
+      genre = `="${option}"`
+    }
+
+    this.ms.params = this.ms.params.set('genre', genre)
+    this.findMovies(this.ps.currentPage, this.column, this.order)
+  }
+
+  setShow(option) {
+    this.ms.params = this.ms.params.set('date', option)
     this.findMovies(this.ps.currentPage, this.column, this.order)
   }
 
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from "@angular/core"
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpParams } from "@angular/common/http"
 import { Observable } from "rxjs"
 import { Filmow_API } from './../app.api';
 import { Movie } from './movie.model';
+import { PaginationService } from "./../components/pagination/pagination.service"
 
 @Injectable()
 export class MoviesService {
 
-    constructor(private http: HttpClient) { }
+    params: HttpParams = new HttpParams()
 
-    // getInfoMovies(page: number): Observable<Movie[]> {
-    //     return this.http.get<Movie[]>(`${Filmow_API}/movies/${page}`)
-    // }
+    constructor(private http: HttpClient, private ps: PaginationService) { }
 
     getInfoMovies(page: number, column: string, order: string): Observable<Movie[]> {
-        return this.http.get<Movie[]>(`${Filmow_API}/movies/${page}/${column}/${order}`)
+        return this.http.get<Movie[]>(`${Filmow_API}/movies/${page}/${column}/${order}/`, { params: this.params })
     }
 
     getGenres(): Observable<any[]> {
