e6ac3615e6fd1a86f9d05d10c359bf48dd54e41d
hppod <hppodgornik@gmail.com>
2019-08-19 11:55:03 -0300

    Update project

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,3 +1,3 @@
-export const Filmow_API = 'http://localhost:3000'
+// export const Filmow_API = 'http://localhost:3005'
 
-// export const Filmow_API = 'http://hppodgornik-com.umbler.net'
+export const Filmow_API = 'http://hppodgornik-com.umbler.net'
